9383492536e1fc5835a98d1ef5829d868be4a160
addUsername <60299373+addUsername@users.noreply.github.com>
2020-11-02 17:30:25 +0100

    Add files via upload

--- app/src/main/java/com/example/demo/controller/Controller.java
+++ app/src/main/java/com/example/demo/controller/Controller.java
@@ -4,26 +4,29 @@ import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.demo.services.AemetService;
 import com.example.demo.utils.AemetDTO;
-import com.example.demo.utils.AemetDTOWeek;
 import com.example.demo.utils.AemetDTOWeek2;
 import com.example.demo.utils.Utils;
 
 @org.springframework.stereotype.Controller
+@Scope("session")
 public class Controller {
 
 	@Autowired
 	AemetService aemetService;
+	
+	private int visitCounter = 0;
 	
 	private String choosenProv = "Seleccione una provincia";
 	private String choosenIdema = "Seleccione una estación";
@@ -41,52 +44,54 @@ public class Controller {
 	public void addAttributes(Model model) {
 		
 		if(firstLoad) readJson();		
-		model.addAttribute("provincias", myMap);
+		//model.addAttribute("provincias", myMap);//quitar
 	    model.addAttribute("provinciasCleaned", myPrettyMap);
-	    model.addAttribute("choosenProv", choosenProv);
-	    model.addAttribute("choosenIdema", choosenIdema);
-	    model.addAttribute("weekChart", response);
-	    model.addAttribute("realWeekChart", responseWeek);
-	    model.addAttribute("loadCharts", loadCharts);
 	}
 	
 	@GetMapping("/")
-    public String main(Model model) {
-		//index.html
+    public String main(HttpSession session) {
+		
+		session.setAttribute("loadCharts", loadCharts);
+		session.setAttribute("choosenProv", choosenProv);
+		session.setAttribute("idemas", (myPrettyMap.get(choosenProv) == null)? "Selecione provincia" : myPrettyMap.get(choosenProv));
+		session.setAttribute("choosenIdema", choosenIdema);
+		session.setAttribute("choosenIdemaCod", choosenIdemaCod);
 		firstLoad = false;
 		return "index";
     }
 	
 	@GetMapping("/provincia/{provincia}")
-    public RedirectView provincia(Model model, @PathVariable String provincia) {
+    public RedirectView provincia(HttpSession session, @PathVariable String provincia) {
 		loadCharts = false;
 		choosenProv = provincia;
-		//return "redirect:/";
 		return new RedirectView("/");
     }
 	@GetMapping("/idema/{idema}")
-    public RedirectView idema(Model model, @PathVariable String idema) {
+    public RedirectView idema(HttpSession session, @PathVariable String idema) {
 		loadCharts = false;
 		choosenIdema = idema;
 		TreeMap myTree =  (TreeMap) myPrettyMap.get(choosenProv);
-		choosenIdemaCod = (String) myTree.get(idema);
-		//return "redirect:/";
-		//modelAndView.setViewName("redirect:/");
+		choosenIdemaCod = (String) myTree.get(idema);		
 		return new RedirectView("/");
     }
 	@GetMapping("/show")
-	public String show() {
+	public String show(HttpSession session) {
+		if(!choosenIdema.equals("Toledo"))visitCounter += 1;
+		
 		loadCharts = true;
 		aemetService.setIdema(choosenIdemaCod);
 		Future<AemetDTO[]> dataDay = aemetService.getData();
 		Future<AemetDTOWeek2[]> dataWeek = aemetService.getDataWeek();
 		
 		try {
-			response = dataDay.get();
-			responseWeek = dataWeek.get();
+			session.setAttribute("weekChart",dataDay.get());
+			session.setAttribute("realWeekChart", dataWeek.get());
 		} catch (InterruptedException | ExecutionException e) {
 			e.printStackTrace();
 		}
+		//some statics
+		System.out.println("[ show ] -- Looking for "+choosenProv+" "+choosenIdema);
+		System.out.println("[ show ] -- total count "+ visitCounter);
 		return "redirect:/";
 	}
 	private void readJson() {
@@ -95,5 +100,4 @@ public class Controller {
 		myMap = utils.getMyMap();
 		myPrettyMap = utils.getMyPrettyMap();		
 	}
-
 }
